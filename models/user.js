e62405fa0fe3511df06bc7384fe255f3a61c6260
Alina <49275467+alinaianara@users.noreply.github.com>
2019-05-01 19:13:25 +0200

    adds admin

--- models/user.js
+++ models/user.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose"),
-      passportLocalMongoose = require("passport-local-mongoose");
+  passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+  username: String,
+  password: String,
+  isAdmin: { type: Boolean, default: false }
 });
 
 UserSchema.plugin(passportLocalMongoose);
--- routes/index.js
+++ routes/index.js
@@ -4,48 +4,51 @@ const passport = require("passport");
 const User = require("../models/user");
 
 //root rout
-router.get("/", function(req, res){
-    res.render("landing");
+router.get("/", function(req, res) {
+  res.render("landing");
 });
-
 
 //show register form
-router.get("/register", function (req, res){
-    res.render("register", {page:"register"});
+router.get("/register", function(req, res) {
+  res.render("register", { page: "register" });
 });
 //handle sign up logic
-router.post("/register", function(req, res){
-    const newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-            return res.render("register", {error: err.message});
-        }
-        passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Welcome to YelpCamp " + user.username);
-            res.redirect("/campgrounds");
-        });
+router.post("/register", function(req, res) {
+  const newUser = new User({ username: req.body.username });
+  if (req.body.adminCode === "1234") {
+    newUser.isAdmin = true;
+  }
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("register", { error: err.message });
+    }
+    passport.authenticate("local")(req, res, function() {
+      req.flash("success", "Welcome to YelpCamp " + user.username);
+      res.redirect("/campgrounds");
     });
+  });
 });
 
 //show login form
-router.get("/login", function (req, res){
-    res.render("login", {page:"login"});
+router.get("/login", function(req, res) {
+  res.render("login", { page: "login" });
 });
 //handling login logic
-router.post("/login", passport.authenticate("local", {
+router.post(
+  "/login",
+  passport.authenticate("local", {
     successRedirect: "/campgrounds",
     failureRedirect: "/login"
-    }), function (req, res){
-});
+  }),
+  function(req, res) {}
+);
 
 //logout route
-router.get("/logout", function (req, res){
-    req.logout();
-    req.flash("success", "Logged you out. See you next time!");
-    res.redirect("/campgrounds");
+router.get("/logout", function(req, res) {
+  req.logout();
+  req.flash("success", "Logged you out. See you next time!");
+  res.redirect("/campgrounds");
 });
-
-
 
 module.exports = router;
